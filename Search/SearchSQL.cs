89afe3fb15b78585727750c88d85ebed862128bf
deadandemo <122723676+deadandemo@users.noreply.github.com>
2023-11-17 16:29:36 -0700

    Fixing SQL in SearchSQL

--- Search/SearchSQL.cs
+++ Search/SearchSQL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Automation;
 
 namespace GroupPrject.Search
 {
@@ -10,47 +11,68 @@ namespace GroupPrject.Search
     {
         public SearchSQL() { }
 
+        public string SearchAll()
+        {
+            string sql = "SELECT * FROM Invoices";
+            return sql;
+        }
+
         public string SearchNum(int num)
         {
             string sql = "SELECT * FROM Invoices WHERE InvoiceNum = " + num;
             return sql;
         }
 
-        public string SearchDate(DateTime date)
+        public string SearchNumDate(int num, string date)
         {
-            string sql = "SELECT * FROM Invoices WHERE InvoiceDate = " + date.ToString();
+           string sql = "SELECT * FROM Invoices WHERE InvoiceNum=" + num+ " AND InvoiceDate = #" + date+ "#"; 
+           return sql;
+        }
+
+       public string SearchNumDateCharges(int num, string date, int charges)
+        {
+            string sql = "SELECT * FROM Invoices WHERE InvoiceNum = " + num + " AND InvoiceDate = #" + date + "# AND TotalCost = " + charges;
             return sql;
         }
 
         public string SearchCharges(int charges) 
         {
-            string sql = "SELECT * FROM Invoices WHERE Charges= " + charges;
+            string sql = "SELECT * FROM Invoices WHERE TotalCost = " + charges;
             return sql;
         }
 
-        public string SearchNumDate(int num, DateTime date)
+        public string SearchDateCharges(string date, int charges)
         {
-            string sql = "SELECT * FROM Invoices WHERE InvoiceNum=" + num+ " AND Date = " + date.ToString();
+            string sql = "SELECT * FROM Invoices WHERE InvoiceDate = #" + date + "# AND TotalCost = " + charges;
             return sql;
         }
 
-        public string SearchNumCharges(int num, int charges)
+        public string SearchDate(string date)
         {
-            string sql = "SELECT * FROM Invoices WHERE InvoiceNum= " + num+ " AND Charges = "+ charges;
+            string sql = "SELECT * FROM Invoices WHERE InvoiceDate = #" + date + "#";
             return sql;
         }
 
-        public string SearchDateCharges(DateTime date, double[] charges)
+        public string SearchDistinctNum(int num)
         {
-            string sql = "SELECT * FROM Invoices WHERE Date = " + date.ToString() + " AND Charges = " + charges;
+            string sql = "SELECT DISTINCT(InvoiceNum) From Invoices order by InvoiceNum";
             return sql;
         }
 
-        public string SearchNumDateCharges(int num, DateTime date, int charges)
+        public string SearchDistinctDate(string date)
         {
-            string sql = "SELECT * FROM Invoices WHERE InvoiceNum = " + num + " AND Date = " + date.ToString() + " AND Charges =" + charges;
+            string sql = "SELECT DISTINCT(InvoiceDate) From Invoices order by InvoiceDate";
             return sql;
         }
+
+        public string SearchDistinctCost(int charges)
+        {
+            string sql = "SELECT DISTINCT(TotalCost) From Invoices order by TotalCost";
+            return sql;
+        }
+
+
+ 
 
 
 
